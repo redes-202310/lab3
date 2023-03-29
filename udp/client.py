23e2df513578249eb4085dad3e4987d5ffd5fefa
mar-cas3 <m.castroi@uniandes.edu.co>
2023-03-29 08:27:06 -0500

    add other requierements
    falta todavia llenar el log y que client.py mande todos los clientes (no toque correr c/u en la terminal)

--- udp/client.py
+++ udp/client.py
@@ -1,4 +1,6 @@
 import socket
+import os
+import time
 
 ip_address = "127.0.0.1" # Change this to the IP address of your machine
 port_number = 8000
@@ -10,14 +12,40 @@ udp_socket.sendto(b"send_file", (ip_address, port_number))
 
 FRAGMENT_SIZE = 8000
 
+files_dir = "udp/ArchivosRecibidos/"
+if not os.path.exists(files_dir):
+    os.makedirs(files_dir)
+
+log_dir = "udp/Logs/Client/"
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
+current_time = time.strftime("%Y-%m-%d_%H-%M-%S")
+log_file = open(log_dir + current_time + ".log", "w")
+
 chunk_data, server_addr = udp_socket.recvfrom(FRAGMENT_SIZE)
 print(f"Received {len(chunk_data)} bytes from {server_addr}")
 print(f"Chunk data: {chunk_data}")
 
 if chunk_data == b"ACK":
-    print("Received ACK")
+    print("You have succesfully connected to the server.")
+
+chunk_data, server_addr = udp_socket.recvfrom(FRAGMENT_SIZE)
+print(f"Received {len(chunk_data)} bytes from {server_addr}")
+print(f"Chunk data: {chunk_data}")
+
+if chunk_data == b"All clients have connected, preparing to send file.":
+    print("All clients have connected, preparing to send file.")    
+
+
+
+num_client = "XX" #TODO: get the client number from the server
+test_num = "XX" #TODO: get the test number from the server
+
+file_name = f"{files_dir}Cliente{num_client}-Prueba{test_num}.txt"
+
 # open a file to write the received data to
-with open('received_file.txt', 'wb') as file:
+with open(file_name, 'wb') as file:
     while True:
         # receive a chunk of data
         
--- udp/server.py
+++ udp/server.py
@@ -1,29 +1,49 @@
 import socket
-import select
+import threading
 import os
+import time
 
 # Create a UDP socket
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Bind the socket to a specific IP address and port number
-ip_address = "127.0.0.1" # Change this to the IP address of your machine
-port_number = 8000 # Change this to any available port number
+ip_address = "127.0.0.1" 
+port_number = 8000 
 udp_socket.bind((ip_address, port_number))
 
+FRAGMENT_SIZE = 8000  # TODO: ask if KB is Kilo or Kibi
 
 file_paths = {"file_100MB": "data/file-100.txt", "file_250MB": "data/file-250.txt", "lorem": "data/lorem.txt"}
 file_sizes = {"file_100MB": os.path.getsize(file_paths["file_100MB"]), "file_250MB": os.path.getsize(file_paths["file_250MB"]), "lorem": os.path.getsize(file_paths["lorem"])}
 
-FRAGMENT_SIZE = 8000  # TODO: ask if KB is Kilo or Kibi
+
+print(os.getcwd())
+# Initialize the log file directory
+log_dir = "udp/Logs/Server/"
+
+# Create the log file directory if it does not exist
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
+# Initialize the log file
+current_time = time.strftime("%Y-%m-%d_%H-%M-%S")
+log_file = open(log_dir + current_time + ".log", "w")
+
+
 
 # Function to send a file to a client
-def send_file(client_address, file_path):
+def send_file(client_address, client_num, file_path):
+    print("Sending file to client", client_num)
     # Open the requested file and read its data
+    start_time = time.time()
     with open(file_path, "rb") as f:
         data = f.read(FRAGMENT_SIZE)
         while data:
             if udp_socket.sendto(data, client_address):
                 data = f.read(FRAGMENT_SIZE)
+    end_time = time.time()
+    execution_time = end_time - start_time
+    log_file.write(f"Transfer time for client {client_num}: {execution_time}s\n")
 
 
 file_name = input("Enter the name of the file to send (file_100MB or file_250MB): ")
@@ -40,7 +60,7 @@ if file_name in file_paths:
         while num_connected < num_clients:
             try:
                 request_data, client_address = udp_socket.recvfrom(1024)
-            except:
+            except Exception as exp:
                 request_data = False
             if request_data:
                 if request_data == b"send_file":
@@ -50,20 +70,28 @@ if file_name in file_paths:
                     num_connected += 1
                 
         # all have connected
-        print("All clients have connected")
+        print("All clients have connected, preparing to send file.")
         for client_address in client_sockets:
             if client_address != udp_socket:
-                send_file(client_address, file_path)
+                print("Sending to", client_address)
+                udp_socket.sendto(b"All clients have connected, preparing to send file.", client_address)
+        
+        client_num = 1
+        log_file.write(f"File name: {file_name}\n")
+        log_file.write(f"File size: {file_sizes[file_name]} bytes\n")
+        for client_address in client_sockets:
+            if client_address != udp_socket:
+                thread = threading.Thread(target=send_file, args=(client_address, client_num, file_path))
+                thread.start()
+                client_num += 1
             # read_socket, _, _ = select.select(client_sockets, [], [])
-
-            # for sock in read_socket:
-            #     if sock == udp_socket:
-            #         request_data, client_address = udp_socket.recvfrom(1024)
-            #         print("received request from client", client_address)
-            #         client_sockets.append(client_address)
-            #         num_connected += 1
-            #     else:
-            #         client_address = sock
-            #         send_file(client_address, file_path)
+        
+        active_threads = threading.active_count()
+        print("ACTIVE:", threading.active_count())
+        while active_threads > 1:
+            active_threads = threading.active_count()
+        print("ACTIVE:", threading.active_count())
+        print("All files sent")
+        log_file.close()
     else:
         print("Invalid number of clients")
